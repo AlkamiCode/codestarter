1016f7ec9ad7cc71e5ff1881a2165fca32db203b
Marla Brizel <marlabrizel@gmail.com>
2015-08-25 13:51:44 -0600

    fix errant migration

--- db/migrate/20150824230118_add_item_id_to_order_items.rb
+++
@@ -1,7 +0,0 @@
-class AddItemIdToOrderItems < ActiveRecord::Migration
-  def change
-    change_table :order_items do |t|
-      t.references :item, index: true, foreign_key: true
-    end
-  end
-end
