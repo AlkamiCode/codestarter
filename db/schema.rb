07eea71f453d1a6175a6be6bfc7ff91ba284b0ca
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-08-25 16:34:32 -0600

    generate projects table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150825221450) do
+ActiveRecord::Schema.define(version: 20150825221858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,18 @@ ActiveRecord::Schema.define(version: 20150825221450) do
   end
 
   add_index "orders", ["user_id"], name: "index_orders_on_user_id", using: :btree
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.string   "image"
+    t.integer  "company_id"
+    t.integer  "current_funding"
+    t.integer  "funding_goal"
+    t.datetime "end_date"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   create_table "sales", force: :cascade do |t|
     t.string   "name"
