d3add52fba2015b47e34bf0eb3595eae01023c02
Marla Brizel <marlabrizel@gmail.com>
2015-08-26 08:57:20 -0600

    Projects Index Page
    Custom styling with bootstrap for projects index page
    Rough feature test
    Hook up database model to view, with some hard coded-values to be filled
    in later (percentage of funding goal, etc.)

--- spec/features/user_can_view_projects_spec.rb
+++ spec/features/user_can_view_projects_spec.rb
@@ -1,16 +1,18 @@
 require 'rails_helper'
 
 RSpec.feature "UserCanViewProjects", type: :feature do
-  let!(:company1) { Company.create!(name: "Ocean World", description: "the sea")}
-  let!(:company2) { Company.create!(name: "Turing", description: "teh codez")}
-  let!(:project1) { Project.create!(name: "A name", description: "best project ever") }
-  let!(:project2) { Project.create!(name: "Another project", description: "ok project") }
-  
+  # let!(:company1) { Company.create!(name: "Ocean World", description: "the sea")}
+  # let!(:company2) { Company.create!(name: "Turing", description: "teh codez")}
+  let!(:project1) { Project.create!(name: "A name", description: "best project ever", current_funding: 500) }
+  let!(:project2) { Project.create!(name: "Another project", description: "ok project", current_funding: 25) }
+
   it "can view all projects from the projects page" do
     visit projects_path
     expect(page).to have_content("A name")
     expect(page).to have_content("Another project")
     expect(page).to have_content("best project ever")
     expect(page).to have_content("ok project")
+    expect(page).to have_content("Current Funding: $500.00")
+    expect(page).to have_content("Current Funding: $25.00")
   end
 end
