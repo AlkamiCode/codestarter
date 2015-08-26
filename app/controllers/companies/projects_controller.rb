db201bc1967833796e5dd9683e64e2d608eb6c57
Marla Brizel <marlabrizel@gmail.com>
2015-08-26 15:33:29 -0600

    create namespaced controllers for companies/projects

--- app/controllers/companies/projects_controller.rb
+++ app/controllers/companies/projects_controller.rb
@@ -1,2 +1,6 @@
-class Companies::ProjectsController < ApplicationController
+class Companies::ProjectsController < Companies::CompaniesController
+  def index
+    company = Company.find_by(url: params[:company])
+    @projects = company.projects
+  end
 end
