efcfd7051dfd14ef9b6ba21f222264946844e796
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-08-26 11:46:42 -0600

    fix broken specs

--- spec/features/guest_user_visits_single_project_spec.rb
+++ spec/features/guest_user_visits_single_project_spec.rb
@@ -3,11 +3,14 @@ require "rails_helper"
 RSpec.describe "guest can visit single projects", type: :feature do
   context "a guest user" do
     let!(:project) { Fabricate(:project) }
+    let!(:project_2) { Fabricate(:project, name: "second name") }
+    let!(:project_3) { Fabricate(:project, name: "third name") }
+    let!(:project_4) { Fabricate(:project, name: "fourth name") }
 
     it "can visit a single project" do
       visit projects_path
 
-      click_link "Sample Project"
+      first(:link, "View Project").click
       expect(current_path).to eq project_path(project)
 
       within(".header") do
@@ -18,9 +21,8 @@ RSpec.describe "guest can visit single projects", type: :feature do
 
       within ".project-details" do
         within ".col-md-4" do
-          expect(page).to have_content "265 backers"
-          expect(page).to have_content "$1000 current funding"
-          expect(page).to have_content "$2000 pledged goal"
+          expect(page).to have_content "$200.00"
+          expect(page).to have_content "pledged of $500.00 goal"
         end
       end
     end
