cac599072ce422231e5bf70533c68ae9c58c2593
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-08-25 21:24:23 -0600

    add validations in project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,12 @@
 class Project < ActiveRecord::Base
+  belongs_to :company
+
+  validates :description,
+            :current_funding,
+            :funding_goal,
+            :end_date,
+            :name,
+            :image,
+            :company_id, presence: true
+  validates :name, :image, :company_id, uniqueness: true
 end
