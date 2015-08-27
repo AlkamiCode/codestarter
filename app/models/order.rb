4185fa73cad04ff36c5df7800e77af5f6db27f69
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-08-27 15:17:21 -0600

    update how order items are built

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,7 @@
 class Order < ActiveRecord::Base
   belongs_to :user
   has_many :order_items
-  has_many :items, through: :order_items
-
+  has_many :projects, through: :order_items
   before_create :create_from_cart
 
   enum status: %w(ordered paid cancelled completed)
@@ -11,7 +10,7 @@ class Order < ActiveRecord::Base
 
   def total
     order_items.inject(0) do |subtotal, order_item|
-      subtotal + (order_item.item.final_price * order_item.quantity)
+      subtotal + order_item.funding_amount
     end
   end
 
@@ -19,8 +18,10 @@ class Order < ActiveRecord::Base
 
   def create_from_cart
     if cart_data
-      cart_data.each do |item_id, quantity|
-        order_items.build(quantity: quantity, item_id: item_id.to_i, order_id: id)
+      cart_data.each do |project_id, funding_amount|
+        company_id = Project.find(project_id).company_id
+        order_items.build(funding_amount: funding_amount,
+          project_id: project_id.to_i, order_id: id, company_id: company_id)
       end
     end
   end
