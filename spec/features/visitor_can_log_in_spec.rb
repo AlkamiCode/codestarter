1d05c0b46b6241438df54f264a4793b8d5f12bad
Marla Brizel <marlabrizel@gmail.com>
2015-08-27 11:10:15 -0600

    finish feature test

--- spec/features/visitor_can_log_in_spec.rb
+++ spec/features/visitor_can_log_in_spec.rb
@@ -13,6 +13,27 @@ RSpec.feature "Registered user can log in", type: :feature do
         fill_in "Password", with: "password"
         click_button "Log In"
       end
+
+      expect(current_path).to eq root_path
+      expect(page).to have_content "Logout"
+
+      within(".nav") do
+        expect(page).not_to have_content "Log In"
+        expect(page).not_to have_content "Sign Up"
+      end
+    end
+
+    it "can't log in with invalid params" do
+      visit root_path
+      click_link "Log In"
+
+      within (".login-modal") do
+        fill_in "Username", with: "marla"
+        fill_in "Password", with: "wrongpass"
+        click_button "Log In"
+      end
+
+      expect(page).to have_content "Invalid username or password"
     end
   end
 end
