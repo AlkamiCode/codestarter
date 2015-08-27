0db8b9b7ba1a51110a157da49605cbece4304a41
Dmitry Vizersky <dmitry.vizer@gmail.com>
2015-08-26 18:18:18 -0600

    fixed user home page test to expect links to be inside navbar

--- spec/features/user_visits_home_page_spec.rb
+++ spec/features/user_visits_home_page_spec.rb
@@ -4,10 +4,12 @@ RSpec.feature User, type: :feature do
   scenario "visits landing page and sees links" do
     visit root_path
 
-    expect(page).to have_link("Sign Up")
-    expect(page).to have_link("Login")
-    expect(page).to have_link("About")
-    expect(page).to have_link("Contact")
+    within(".navbar") do
+      expect(page).to have_link("Sign Up")
+      expect(page).to have_link("Login")
+      expect(page).to have_link("About")
+      expect(page).to have_link("Contact")
+    end
     expect(page).to have_link("Featured Projects")
   end
 end
