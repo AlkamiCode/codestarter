05dbe48693b0211130081aa040e02c968c7423c5
Dmitry Vizersky <dmitry.vizer@gmail.com>
2015-08-26 18:30:05 -0600

    user can click on featured project and visit project show page

--- spec/features/user_visits_featured_project_spec.rb
+++ spec/features/user_visits_featured_project_spec.rb
@@ -1,9 +1,12 @@
 require "rails_helper"
 
 RSpec.feature User, type: :feature do
-  let!(:project) { Fabricate(:project) }
+  let!(:project)   { Fabricate(:project) }
   let!(:project_2) { Fabricate(:project, name: "second name") }
   let!(:project_3) { Fabricate(:project, name: "third name") }
+  let!(:project_4) { Fabricate(:project, name: "fourth name") }
+  let!(:project_5) { Fabricate(:project, name: "fifth name") }
+  let!(:project_6) { Fabricate(:project, name: "sixth name") }
 
   scenario "views featured projects on home page" do
     visit root_path
@@ -12,5 +15,11 @@ RSpec.feature User, type: :feature do
   end
 
   scenario "visits featured projects show page" do
+    visit root_path
+
+    click_link(project_2.name)
+
+    expect(current_path).to eq project_path(project_2.id)
+    expect(page).to have_content(project_2.name)
   end
 end
