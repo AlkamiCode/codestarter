838b09d6418dd576dfe0a0a86c1e44da4f614ced
Dmitry Vizersky <dmitry.vizer@gmail.com>
2015-08-27 01:52:33 -0600

    User successfully registers from any page - link to modal in the nav-bar - created modal for user registration - extracted modal into partial - styled modal - created feature test for registration - added validations to user model

--- spec/features/registered_user_adds_cart_projects_spec.rb
+++ spec/features/registered_user_adds_cart_projects_spec.rb
@@ -1,9 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "Registered user", type: :feature do
-  let!(:project) { Fabricate(:project) }
-  let!(:project_2) { Fabricate(:project, name: "Second Project") }
-
+RSpec.feature "User", type: :feature do
   it "can register successfully" do
     visit root_path
 
@@ -15,8 +12,5 @@ RSpec.feature "Registered user", type: :feature do
         click_button "Create Account"
       end
     end.to change { User.count }.from(0).to(1)
-  end
-
-  it "can add projects to cart" do
   end
 end
