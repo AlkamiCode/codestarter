61c9ae2bed4060a715ce67240559ca79ff35508d
Dmitry Vizersky <dmitry.vizer@gmail.com>
2015-08-27 02:01:38 -0600

    add feature test for registration from cart page

---
+++ spec/features/user_registers_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.feature "User", type: :feature do
+  it "can register successfully from home page" do
+    visit root_path
+
+    expect do
+      within (".register-modal") do
+        fill_in "Username", with: "Lovisa"
+        fill_in "Password", with: "rocks"
+        fill_in "Password confirmation", with: "rocks"
+        click_button "Create Account"
+      end
+    end.to change { User.count }.from(0).to(1)
+  end
+
+  it "can register successfully from cart page" do
+    visit cart_path
+
+    expect do
+      within (".register-modal") do
+        fill_in "Username", with: "Lovisa"
+        fill_in "Password", with: "rocks"
+        fill_in "Password confirmation", with: "rocks"
+        click_button "Create Account"
+      end
+    end.to change { User.count }.from(0).to(1)
+  end
+end
