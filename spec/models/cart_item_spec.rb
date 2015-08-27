815910e1600cc004245f93b50e65ac9c3c697f27
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-08-27 11:43:02 -0600

    cart item spec passing

--- spec/models/cart_item_spec.rb
+++ spec/models/cart_item_spec.rb
@@ -3,33 +3,35 @@ require "rails_helper"
 RSpec.describe CartItem, type: :model do
   let!(:project) { Fabricate(:project) }
 
+  before(:each) do
+    @cart_item = CartItem.new(project)
+  end
+
   it "returns the title of the item" do
-    cart_item = CartItem.new(project)
-    expect(cart_item.title).to eq("Item 1")
+    expect(@cart_item.name).to eq("Sample Project")
   end
 
   it "returns the description of the item" do
-    cart_item = CartItem.new(project)
-    expect(cart_item.description).to eq("description")
+    expect(@cart_item.description).to eq("To make the sample world a better place.")
   end
 
-  it "returns the price of the item" do
-    cart_item = CartItem.new(project)
-    expect(cart_item.price).to eq(400)
+  it "returns the image url" do
+    expect(@cart_item.image).to eq("hello.lpg")
   end
 
-  it "returns the image of the item" do
-    cart_item = CartItem.new(project)
-    expect(cart_item.image).to eq("some url")
+  it "returns the current funding" do
+    expect(@cart_item.current_funding).to eq(200)
   end
 
-  it "returns the item quantity" do
-    cart_item = CartItem.new(project, 2)
-    expect(cart_item.quantity).to eq(2)
+  it "returns the funding goal" do
+    expect(@cart_item.funding_goal).to eq(500)
   end
 
-  it "defaults the item quantity to 0" do
-    cart_item = CartItem.new(project)
-    expect(cart_item.quantity).to eq(0)
+  it "has an end date" do
+    expect(@cart_item.end_date).to eq("2015-12-12")
+  end
+
+  it "funding amount defaults to 0" do
+    expect(@cart_item.funding_amount).to eq 0
   end
 end
