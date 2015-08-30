5c221055d9d60e9fecd881e1f8ddd9b68fe668fa
Marla Brizel <marlabrizel@gmail.com>
2015-08-30 16:13:45 -0600

    more model tests for permissions

--- spec/models/permission_service_spec.rb
+++ spec/models/permission_service_spec.rb
@@ -376,6 +376,242 @@ RSpec.describe "PermissionService", type: :model do
   end
 
   context "as a company admin" do
+    let(:user) { Fabricate(:user, roles: %w(company_admin)) }
 
+    context "visiting the home page" do
+      let(:controller) { "homepage" }
+
+      context "looking at homepage#index" do
+        let(:action) { "index" }
+
+        it "allows access" do
+          expect(allowed).to be true
+        end
+      end
+
+      context "visiting companies" do
+        let(:controller) { "companies" }
+
+        context "looking at companies#index" do
+          let(:action) { "index" }
+
+          it "allows access" do
+            expect(allowed).to be true
+          end
+        end
+
+        context "looking at companies#show" do
+          let(:action) { "show" }
+
+          it "allows access" do
+            expect(allowed).to be true
+          end
+        end
+      end
+
+      context "logging in or out" do
+        let(:controller) { "sessions" }
+
+        context "looking at sessions#new" do
+          let(:action) { "new" }
+
+          it "allows access" do
+            expect(allowed).to be true
+          end
+        end
+
+        context "looking at sessions#create" do
+          let(:action) { "create" }
+
+          it "allows access" do
+            expect(allowed).to be true
+          end
+        end
+
+        context "looking at sessions#destroy" do
+          let(:action) { "destroy" }
+
+          it "allows access" do
+            expect(allowed).to be true
+          end
+        end
+      end
+
+      context "visiting projects" do
+        let(:controller) { "projects" }
+
+        context "looking at projects#index" do
+          let(:action) { "index" }
+
+          it "allows access" do
+            expect(allowed).to be true
+          end
+        end
+
+        context "looking at projects#show" do
+          let(:action) { "show" }
+
+          it "allows access" do
+            expect(allowed).to be true
+          end
+        end
+      end
+
+      context "visiting companies/projects" do
+        let(:controller) { "companies/projects" }
+
+        context "looking at companies/projects#index" do
+          let(:action) { "index" }
+
+          it "allows access" do
+            expect(allowed).to be true
+          end
+        end
+
+        context "looking at companies/projects#show" do
+          let(:action) { "show" }
+
+          it "allows access" do
+            expect(allowed).to be true
+          end
+        end
+
+        context "looking at companies/projects#new" do
+          let(:action) { "new" }
+
+          it "allows access" do
+            expect(allowed).to be true
+          end
+        end
+
+        context "looking at companies/projects#create" do
+          let(:action) { "create" }
+
+          it "allows access" do
+            expect(allowed).to be true
+          end
+        end
+
+        context "looking at companies/projects#edit" do
+          let(:action) { "edit" }
+
+          it "allows access" do
+            expect(allowed).to be true
+          end
+        end
+
+        context "looking at companies/projects#update" do
+          let(:action) { "update" }
+
+          it "allows access" do
+            expect(allowed).to be true
+          end
+        end
+
+        context "looking at companies/projects#destroy" do
+          let(:action) { "destroy" }
+
+          it "allows access" do
+            expect(allowed).to be true
+          end
+        end
+      end
+
+      context "visiting account" do
+        let(:controller) { "users" }
+
+        context "looking at profile information" do
+          let(:action) { "show" }
+
+          it "allows access" do
+            expect(allowed).to be true
+          end
+        end
+
+        context "updating profile information" do
+          let(:action) { "update" }
+
+          it "allows access" do
+            expect(allowed).to be true
+          end
+        end
+      end
+
+      context "adding, removing, and updating items in the cart" do
+        let(:controller) { "cart_items" }
+
+        context "looking at cart_items#index" do
+          let(:action) { "index" }
+
+          it "allows access" do
+            expect(allowed).to be true
+          end
+        end
+
+        context "looking at cart_items#create" do
+          let(:action) { "create" }
+
+          it "allows access" do
+            expect(allowed).to be true
+          end
+        end
+
+        context "looking at cart_items#update" do
+          let(:action) { "update" }
+
+          it "allows access" do
+            expect(allowed).to be true
+          end
+        end
+
+        context "looking at cart_items#destroy" do
+          let(:action) { "destroy" }
+
+          it "allows access" do
+            expect(allowed).to be true
+          end
+        end
+      end
+
+      context "visiting orders" do
+        let(:controller) { "orders" }
+
+        context "looking at orders#index" do
+          let(:action) { "index" }
+
+          it "allows access" do
+            expect(allowed).to be true
+          end
+        end
+
+        context "looking at orders#create" do
+          let(:action) { "create" }
+
+          it "allows access" do
+            expect(allowed).to be true
+          end
+        end
+
+        context "looking at orders#show" do
+          let(:action) { "show" }
+
+          it "allows access" do
+            expect(allowed).to be true
+          end
+        end
+      end
+
+      context "signing up for another account" do
+        let(:controller) { "users" }
+
+        context "looking at users#create" do
+          let(:action) { "create" }
+
+          it "denies access" do
+            expect(allowed).to be nil
+          end
+        end
+      end
+    end
   end
 end
