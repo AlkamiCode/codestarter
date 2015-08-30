443f3e0e70beb9cce61672dde09aeabba79b4e86
Dmitry Vizersky <dmitry.vizer@gmail.com>
2015-08-30 17:11:45 -0600

    Registered User fully funds a project - if a registered user fully funds a project that project transitions to "funded" after checking out - passing a funding_amount param after clicking checkout on cart page - updating current_funding of project after checking out - showing only active project in projects index page - showing all company projects, including funded, in /company/projects page

---
+++ spec/features/registered_user_fully_funds_project_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+
+RSpec.describe "registered user fully funds project", type: :feature do
+  let!(:company) { Fabricate(:company) }
+  let!(:project) { Fabricate(:project) }
+  let!(:user) { Fabricate(:user, roles: %w(registered_user)) }
+
+  scenario "project transitions from active to funded" do
+    login_as(user, root_path)
+
+    click_link "Projects"
+    expect(current_path).to eq projects_path
+
+    first(:link, "Fund This Project").click
+    page.find("a[href='/cart']").click
+
+    within(".cart-options") do
+      fill_in 'funding_amount', with: "300"
+      click_button "Set Amount"
+    end
+
+    expect(page).to have_content "$300.00"
+    expect(project.status).to eq("active")
+
+    click_link "Checkout"
+    project.reload
+
+    expect(current_path).to eq orders_path
+    expect(project.status).to eq("funded")
+
+    visit projects_path
+    expect(page).to_not have_content(project.name)
+  end
+end
