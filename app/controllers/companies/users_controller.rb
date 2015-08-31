753fc3de6b50c5bb286292f524d12e21f0b1e082
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-08-30 18:13:09 -0600

    add member route "search", permission and action

--- app/controllers/companies/users_controller.rb
+++ app/controllers/companies/users_controller.rb
@@ -1,5 +1,16 @@
 class Companies::UsersController < Companies::CompaniesController
   def index
+  end
 
+  def new
+  end
+
+  def create
+    require 'pry'; binding.pry
+  end
+
+  def search
+    @user = User.find_by(username: params[:searchfield])
+    render :new
   end
 end
