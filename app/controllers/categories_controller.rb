00548a63969dfbb295db0012fc0e46739c73f2f2
Marla Brizel <marlabrizel@gmail.com>
2015-08-31 19:55:13 -0600

    category show view

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,17 +1,13 @@
 class CategoriesController < ApplicationController
-  before_action :find_company
   before_action :find_category
 
   def show
+    @sorted_projects = @category.projects
   end
 
   private
 
   def find_category
     @category = Category.find_by(slug: params[:id])
-  end
-
-  def find_company
-    @company = Company.where(url: params[:company]).first!
   end
 end
