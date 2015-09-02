ec83df1fde1f19be791e0ddc3b9f619e582f3ad9
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-09-02 17:10:01 -0600

    user recieves an email on creation, funds a project, added as collaborator

--- spec/mailers/notification_mailer_spec.rb
+++ spec/mailers/notification_mailer_spec.rb
@@ -1,10 +1,32 @@
 require "rails_helper"
 
 RSpec.describe NotificationMailer, type: :mailer do
-  context "when a user registers" do
-    let(:user) { Fabricate.build(:user) }
+  let(:user) { Fabricate.build(:user) }
 
+  context "when a user registers" do
     it "receives an email confirming successful registration" do
+      user.save!
+      NotificationMailer.contact(user.email).deliver_now
+      email = ActionMailer::Base.deliveries.last
+
+      expect(email.to.first).to eq(user.email)
+      expect(email.subject).to eq("Welcome to CodeStarter")
+    end
+  end
+
+  context "when a user is added as a collaborator" do
+    it "receives an email confirming that they were added as collaborator" do
+      user.save!
+      NotificationMailer.contact(user.email).deliver_now
+      email = ActionMailer::Base.deliveries.last
+
+      expect(email.to.first).to eq(user.email)
+      expect(email.subject).to eq("Welcome to CodeStarter")
+    end
+  end
+
+  context "when a user funds the project" do
+    it "receives an email confirming that they successfully funded the project" do
       user.save!
       NotificationMailer.contact(user.email).deliver_now
       email = ActionMailer::Base.deliveries.last
