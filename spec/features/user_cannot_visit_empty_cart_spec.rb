ea45a7bd449cd15bd05f9167bc61312cc62f707a
Dmitry Vizersky <dmitry.vizer@gmail.com>
2015-09-01 21:49:47 -0600

    add cart method in rails helper to initialize a cart for tests

--- spec/features/user_cannot_visit_empty_cart_spec.rb
+++ spec/features/user_cannot_visit_empty_cart_spec.rb
@@ -6,22 +6,30 @@ RSpec.describe "user does not have link to empty cart", type: :feature do
   let!(:project) { Fabricate(:project, company_id: company.id) }
 
   scenario "can only visit cart page when cart has an item" do
+    session = {}
+
     visit root_path
-binding.pry
-    expect(page).to_not have_link("a[href='/cart']")
+
+    page.find("#cart").click
+
+    expect(current_path).to eq(root_path)
 
     # login_as(user, root_path)
 
     click_link "Projects"
 
     expect(current_path).to eq projects_path
-    expect(page).to_not have_link("a[href='/cart']")
-
     first(:link, "Fund This Project").click
 
-    page.find("a[href='/cart']").click
+    page.find("#cart").click
 
     expect(current_path).to eq cart_path
+    binding.pry
+
+
+    # page.find("a[href='/cart']").click
+
+    # expect(current_path).to eq cart_path
 
     # within(".cart-options") do
     #   fill_in 'funding_amount', with: "300"
