8fbe36c566efbaffc48286e252e9cfeabf9383c1
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-09-01 20:55:33 -0600

    add test for company admin deleting projects

--- spec/features/company_admin_deletes_project_spec.rb
+++ spec/features/company_admin_deletes_project_spec.rb
@@ -6,23 +6,39 @@ RSpec.describe "Company admin can delete project", type: :feature do
   let!(:project2) { Fabricate(:project, name: "Another Project", company_id: company.id) }
   let!(:user) { Fabricate(:user, company_id: company.id, roles: %w(company_admin)) }
 
-  context "as a company admin" do
-    it "can delete a project" do
+  context "can delete a project" do
+    it "from the companies/projects page" do
       login_as(user, root_path)
       expect(current_path).to eq root_path
 
-      click_on "Account"
-      click_on "#{company.name}'s dashboard"
-      click_on "All Projects"
+      click_link "Account"
+      click_link "#{company.name}'s dashboard"
+
+      click_link "All Projects"
 
       expect(current_path).to eq company_projects_path(company: company.url)
-      expect(page).to have_content "Sample Project"
-      expect(page).to have_content "Another Project"
+      expect(page).to have_content project1.name
+      expect(page).to have_content project2.name
 
       first(:link, "Delete").click
       expect(current_path).to eq company_projects_path(company: company.url)
-      expect(page).not_to have_content "Sample Project"
-      expect(page).to have_content "Another Project"
+      expect(page).to have_content "#{project1.name} successfully removed."
+      expect(page).to have_content project2.name
+    end
+
+    it "form the projects page" do
+      login_as(user, root_path)
+      expect(current_path).to eq root_path
+
+      click_link "Projects"
+      first(:link, "View Project").click
+
+      expect(page).to have_content project1.name
+      first(:link, "Delete").click
+
+      expect(current_path).to eq company_projects_path(company: company.url)
+      expect(page).to have_content "#{project1.name} successfully removed."
+      expect(page).to have_content project2.name
     end
   end
 end
