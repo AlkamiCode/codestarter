defa5b6ebc5a5577d9fa7a18e9209c1fee1fdc41
Marla Brizel <marlabrizel@gmail.com>
2015-09-02 14:35:25 -0600

    remove unused method

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -6,7 +6,7 @@ class CartItem < SimpleDelegator
     @funding_amount = funding_amount
   end
 
-  def self.funding_amounts
-    [100, 200, 500, 1000, 1500, 2000, 2500, 3000]
-  end
+  # def self.funding_amounts
+  #   [100, 200, 500, 1000, 1500, 2000, 2500, 3000]
+  # end
 end
