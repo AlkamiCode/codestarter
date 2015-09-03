e1c2acd2d54aa6eccf00c3629def9f53a2942c15
Lovisa Svallingson <lovisasvallingson@gmail.com>
2015-09-03 09:58:04 -0600

    remove ternary and back to if/else statement :/

--- app/models/project.rb
+++ app/models/project.rb
@@ -23,9 +23,11 @@ class Project < ActiveRecord::Base
   end
 
   def funding_in_percentage
-    current_funding > funding_goal ?
-                      100 :
-                      ((current_funding.to_f / funding_goal.to_f) * 100).to_i
+    if current_funding > funding_goal
+      100
+    else
+      ((current_funding.to_f / funding_goal.to_f) * 100).to_i
+    end
   end
 
   def pretty_end_date
