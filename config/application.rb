5eb763ecc7eafa633e0dd5e2d2fa8755e8592d19
Dmitry Vizersky <dmitry.vizer@gmail.com>
2016-05-31 18:44:41 -0600

    update ActionMailer to user Gmail

--- config/application.rb
+++ config/application.rb
@@ -14,16 +14,27 @@ Bundler.require(*Rails.groups)
 
 module JurassicParka
   class Application < Rails::Application
-    config.action_mailer.delivery_method = :smtp
+    # config.action_mailer.delivery_method = :smtp
 
+    # config.action_mailer.smtp_settings = {
+    #   address:                "smtp.mandrillapp.com",
+    #   port:                   587,
+    #   domain:                 ENV['mandrill_domain'],
+    #   user_name:              "lovisasvallingson@gmail.com",
+    #   password:               ENV['mandrill_api_key'],
+    #   authentication:         'plain',
+    #   enable_starttls_auto:   true,
+    # }
+
+    config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
-      address:                "smtp.mandrillapp.com",
-      port:                   587,
-      domain:                 ENV['mandrill_domain'],
-      user_name:              "lovisasvallingson@gmail.com",
-      password:               ENV['mandrill_api_key'],
-      authentication:         'plain',
-      enable_starttls_auto:   true,
+        :address              => 'smtp.gmail.com',
+        :port                 => 587,
+        :domain               => 'gmail.com',
+        :user_name            => ENV['gmail_username'],
+        :password             => ENV['gmail_password'],
+        :authentication       => 'login',
+        :enable_starttls_auto => true
     }
 
     config.active_record.raise_in_transactional_callbacks = true
